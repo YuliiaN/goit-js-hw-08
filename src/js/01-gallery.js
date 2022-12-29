414ee9afa240f43d9a3d532252c3bea3a1f2a792
Yuliya N <y.sokolova.liya@gmail.com>
2022-12-29 03:47:34 +0200

    incorrect third task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+const divWrapperRef = document.querySelector('.gallery');
+divWrapperRef.innerHTML = addItems(galleryItems);
+
+function addItems(arr) {
+  return arr
+    .map(({ preview, original, description }) => {
+      return `
+<div>
+  <a class="gallery__item" href="${original}">
+    <img 
+    class="gallery__image" 
+    src="${preview}" 
+    alt="${description}"
+  /></a>
+</div>
+`;
+    })
+    .join('');
+}
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,55 @@
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  input: document.querySelector('input'),
+  textarea: document.querySelector('textarea'),
+};
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
 
+refs.input.addEventListener('input', formInput);
+refs.textarea.addEventListener('input', formTextarea);
+// refs.form.addEventListener('input', onFormStorage);
+refs.form.addEventListener('submit', onFormSubmit);
+
+checkSavedValues();
+
+function formInput(event) {
+  const input = event.target.value;
+
+  if (input) {
+    formData[event.target.name] = input;
+  }
+}
+
+function formTextarea(event) {
+  const textarea = event.target.value;
+
+  if (textarea) {
+    formData[event.target.name] = textarea;
+  }
+}
+
+// function onFormStorage(event) {
+//   const formDataJSON = JSON.stringify(formData);
+//   localStorage.setItem(STORAGE_KEY, formDataJSON);
+
+//   formData[event.target.name] = event.target.value;
+//   const formDataJSON = JSON.stringify(formData);
+//   localStorage.setItem(STORAGE_KEY, formDataJSON);
+//   console.log(event.target.value);
+// }
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  event.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function checkSavedValues() {
+  const savedValues = localStorage.getItem(STORAGE_KEY);
+
+  if (savedValues) {
+    refs.input.value = JSON.parse(savedValues).email;
+    refs.textarea.value = JSON.parse(savedValues).message;
+  }
+}
